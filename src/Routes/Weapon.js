7231c295177d25bac1cdb4b683bfc7facf7601c9
kennarddh <kennarddh@gmail.com>
2022-03-31 20:13:58 +0700

    Fix weapon routes

--- src/Routes/Weapon.js
+++ src/Routes/Weapon.js
@@ -20,10 +20,10 @@ import UpdateValidation from '../Validation/Weapon/Update'
 
 const Router = express.Router()
 
-Router.get('/weapons/:id', VerifyJWT, GetAll)
-Router.get('/weapon/:id', [VerifyJWT, ...GetValidation()], GetById)
-Router.post('/weapons/', [VerifyJWT, ...CreateValidation()], Create)
-Router.put('/weapon/:id', [VerifyJWT, ...UpdateValidation()], Update)
-Router.delete('/weapon/:id', [VerifyJWT, ...DeleteValidation()], DeleteById)
+Router.get('/weapons', GetAll)
+Router.get('/weapon/:id', ...GetValidation(), GetById)
+Router.post('/weapons', VerifyJWT, ...CreateValidation(), Create)
+Router.put('/weapon/:id', VerifyJWT, ...UpdateValidation(), Update)
+Router.delete('/weapon/:id', VerifyJWT, ...DeleteValidation(), DeleteById)
 
 export default Router
